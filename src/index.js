05a9894a4d7454ca6a537f92b83ae299eec6b02d
JesusNavarrete215 <Jesus.Navarrete215@outlook.com>
2024-09-04 00:27:28 -0700

    fixing issues with redux toolkit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
-import {createRoot} from 'react-dom/client';
+import { createRoot } from "react-dom/client";
 import React from "react";
-import "./index.css"
+import "./index.css";
 import App from "./App";
+import { Provider } from "react-redux";
+import store from "./app/store";
 
-const root = createRoot(document.getElementById('root'))
-root.render(<React.StrictMode><App/></React.StrictMode>)
+
+const root = createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+       <App /> 
+    </Provider>
+  </React.StrictMode>
+);
