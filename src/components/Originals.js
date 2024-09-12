53c1fd9d4ad1ebb9302f43626a34f1d5b665aca0
JesusNavarrete215 <Jesus.Navarrete215@outlook.com>
2024-09-12 01:26:34 -0700

    set up movie store and now config firestore

---
+++ src/components/Originals.js
@@ -0,0 +1,77 @@
+import styled from "styled-components";
+import {Link} from 'react-router-dom'
+export const Originals = () => {
+  return (
+    <Container>
+      <h4>New to Disney</h4>
+      <Content>
+       <Wrap>
+        <Link to='/'>
+            <img src="https://media.wdwnt.com/2024/08/GUrKIRfXEAEE4jL.jpg" alt="" />
+        </Link>
+       </Wrap>
+       <Wrap>
+        <Link to='/'>
+            <img src="https://media.wdwnt.com/2024/08/GUrKIRfXEAEE4jL.jpg" alt="" />
+        </Link>
+       </Wrap>
+       <Wrap>
+        <Link to='/'>
+            <img src="https://media.wdwnt.com/2024/08/GUrKIRfXEAEE4jL.jpg" alt="" />
+        </Link>
+       </Wrap>
+        <Wrap>
+        <Link to='/'>
+            <img src="https://media.wdwnt.com/2024/08/GUrKIRfXEAEE4jL.jpg" alt="" />
+        </Link>
+       </Wrap>
+
+      </Content>
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  padding: 0 0 26px;
+`;
+
+const Content = styled.div`
+  display: grid;
+  grid-gap: 25px;
+  gap: 25px;
+  grid-template-columns: repeat(4, minmax(0, 1fr));
+
+  @media (max-width: 768px) {
+    grid-template-columns: repeat(2, minmax(0, 1fr));
+  }
+`;
+const Wrap = styled.div`
+  padding-top: 56.25%;
+  border-radius: 10px;
+  box-shadow: rgb(0 0 0 / 80%) 0px 40px 58px -16x, rgb(0 0 0 / 72%) 0px 30px 22px -10px;
+  cursor: pointer;
+  overflow: hidden;
+  position: relative;
+  transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
+  border: 3px solid rgba(249, 249,249, 0.1);
+
+  img{
+    inset: 0px;
+    display:block;
+    height: 100%;
+    position: absolute;
+    opacity: 1;
+    object-fit: cover; 
+    transition: opacity 500ms ease-in-out 0s;
+    width: 100%;
+    z-index: 1;
+    top: 0;
+}
+
+&:hover{
+    box-shadow: rgb(0 0 0 / 80%) 0px 40px 58px -16x, rgb(0 0 0 / 72%) 0px 30px 22px -10px;
+    transform: scale(1.05);
+    border:rgba(249, 249,249, 0.1);
+
+}
+`;
