90cd90e994dca443a54265241da8ac552572f909
JesusNavarrete215 <Jesus.Navarrete215@outlook.com>
2024-09-14 01:51:50 -0700

    finished project

--- src/components/Header.js
+++ src/components/Header.js
@@ -61,7 +61,7 @@ const Header = (props) => {
   return (
   <Nav>
     <Logo>
-      <img src="/images/logo.svg" alt="Disney+" />
+     <a href="/home"><img src="/images/logo.svg" alt="Disney+" /></a> 
     </Logo>
 
     {!userName ? (
